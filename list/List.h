d30769c902566a3025210c448ca7ba3bd7339f22
huangchenyao <hcy_96@outlook.com>
2018-07-09 20:03:03 +0800

    [list整体框架]

--- list/List.h
+++ list/List.h
@@ -18,7 +18,7 @@ protected:
     void init(); // 列表创建时的初始化
     int clear(); // 清除所有节点
     void copyNodes(ListNodePosi<T>, int); // 复制列表中自位置p起的n项
-    void merge(ListNodePosi<T> &, int, List<T> &, ListNodePosi<T>, int); // 有序列表区间归并
+    void merge(ListNodePosi<T>, int, List<T> &, ListNodePosi<T>, int); // 有序列表区间归并
     void mergeSort(ListNodePosi<T> &, int); // 对从p开始连续的n个节点归并排序
     void selectionSort(ListNodePosi<T>, int); // 对从p开始连续的n个节点选择排序
     void insertionSort(ListNodePosi<T>, int); // 对从p开始连续的n个节点插入排序
@@ -50,6 +50,22 @@ public:
     ListNodePosi<T> selectMax() { return selectMax(header->succ, _size); } // 整体最大者
 
     // 可写访问接口
+    ListNodePosi<T> insertAsFirst(T const &e); // 将e当作首节点插入
+    ListNodePosi<T> insertAsLast(T const &e); // 将e当作末节点插入
+    ListNodePosi<T> insertBefore(ListNodePosi<T> p, T const &e); // 将e当作p的前驱插入
+    ListNodePosi<T> insertAfter(ListNodePosi<T> p, T const &e); // 将e当作p的后继插入
+    T remove(ListNodePosi<T> p); // 删除合法位置p处的节点, 返回被删除节点
+    void merge(List<T> &L) { merge(first(), _size, L, L.first(), L._size); } //全列表归并
+    void sort(ListNodePosi<T> p, int n); // 列表区间排序
+    void sort() { sort(first(), _size); } // 列表整体排序
+    int deduplicate(); // 无序去重
+    int uniquify(); // 有序去重
+    void reverse(); // 前后倒置(习题)
+
+    // 遍历
+    void traverse(void (*)(T &)); // 遍历，依次实施visit操作(函数指针，只读局部性修改)
+    template<typename VST>
+    void traverse(VST &); // 遍历，依次实施visit操作(函数对象，可全局性修改)
 };
 
 
--- list/ListNode.h
+++ list/ListNode.h
@@ -6,7 +6,7 @@
 #define DATA_STRUCTURE_LISTNODE_H
 
 typedef int Rank; // 秩
-template <typename T> typedef ListNode<T> * ListNodePosi; // 列表节点位置
+template<typename T> typedef ListNode <T> *ListNodePosi; // 列表节点位置
 
 template<typename T>
 struct ListNode { // 列表节点模板类(以双向链表形式实现)
@@ -19,7 +19,7 @@ struct ListNode { // 列表节点模板类(以双向链表形式实现)
     ListNode() {} // 针对header和trailer的构造
     ListNode(T e, ListNodePosi<T> p = nullptr, ListNodePosi<T> s = nullptr) : data(e), pred(p), succ(s) {} // 默认构造器
 
-            // 操作接口
+    // 操作接口
     ListNodePosi<T> insertAsPred(T const &e); //紧靠当前节点之前插入新节点
     ListNodePosi<T> insertAsSucc(T const &e); //紧随当前节点之后插入新节点
 };
