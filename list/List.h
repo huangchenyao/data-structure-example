a541a18c9ad4add74766bd1a7571bbe9076a5f9e
huangchenyao <hcy_96@outlook.com>
2018-07-10 17:58:40 +0800

    [list，遍历]

--- list/List.h
+++ list/List.h
@@ -242,14 +242,18 @@ void List<T>::reverse() {
 }
 
 template<typename T>
-void List<T>::traverse(void (*)(T &)) {
-
+void List<T>::traverse(void (*visit)(T &)) {
+    for (ListNodePosi<T> p = header->succ; p != trailer; p = p->succ) {
+        visit(p->data);
+    }
 }
 
 template<typename T>
 template<typename VST>
-void List<T>::traverse(VST &) {
-
+void List<T>::traverse(VST &visit) {
+    for (ListNodePosi<T> p = header->succ; p != trailer; p = p->succ) {
+        visit(p->data);
+    }
 }
 
 
