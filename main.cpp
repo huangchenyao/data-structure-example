a947b4e89ce931119a68264af158c23fac6a5ebd
huangchenyao <hcy_96@outlook.com>
2018-07-19 21:46:52 +0800

    [stack，迷宫问题]

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
-#include "stack/Stack.h"
-#include "stack/StackUse.h"
-#include "queen/Queen.h"
 
 int main() {
-//    Vector<char> vector;
-//    vector.insert('(');
-//    vector.remove(0);
-    clock_t startTime, endTime;
-    for (int i = 0; i < 16; ++i) {
-        std::cout << "Queen(" << i << "), ";
-        startTime = clock();
-        std::cout << "solu: " << placeQueens(i) << ", ";
-        endTime = clock();
-        std::cout << "Total Time: " << (double) (endTime - startTime) / CLOCKS_PER_SEC << "s" << std::endl;
+//    clock_t startTime, endTime;
+//    for (int i = 0; i < 16; ++i) {
+//        std::cout << "Queen(" << i << "), ";
+//        startTime = clock();
+//        std::cout << "solu: " << placeQueens(i) << ", ";
+//        endTime = clock();
+//        std::cout << "Total Time: " << (double) (endTime - startTime) / CLOCKS_PER_SEC << "s" << std::endl;
+//
+//    }
 
-    }
     return 0;
 }
